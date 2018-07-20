d92fb9191bd01f43deecf631d693309ddea3e727
dagonyclark <sean.dagony-clark@flatironschool.com>
2018-07-19 21:24:17 -0400

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  dogs = ["Sasha", "Muffin", "Casey", "Jackson"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  dogs = ["Sasha", "Muffin", "Casey", "Jackson"]
+  dogs << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  dogs = ["Sasha", "Muffin", "Casey", "Jackson"]
+  dogs.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  
+  # dogs = ["Sasha", "Muffin", "Casey", "Jackson"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[array.length-1]
 end
+
+# retrieve_last_element_from_array(array = ["wow", "I", "am", "really", "learning", "arrays!"])
